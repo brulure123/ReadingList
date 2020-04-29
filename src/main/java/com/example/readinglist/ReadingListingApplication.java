cbd88120201c58d300e50eaac35ee1f7d6008c6a
brulure123 <benraj.200@gmail.com>
2020-04-29 16:25:10 +0200

    First Commit, ReadingList a spring application with spring boot.

---
+++ src/main/java/com/example/readinglist/ReadingListingApplication.java
@@ -0,0 +1,13 @@
+package com.example.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListingApplication.class, args);
+	}
+
+}
